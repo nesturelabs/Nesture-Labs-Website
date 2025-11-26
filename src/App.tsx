98ed3b91fdc067cc61ce6be8a27f0f5f710b3a78
Thiekshana Perera <thiekshana@phyxle.com>
2025-11-26 11:54:15 +0530

    feat: implement Google Analytics tracking with AnalyticsTracker component and GA helper functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { HelmetProvider } from 'react-helmet-async';
-import { ThemeProvider } from './components/layout/ThemeProvider';
-import { Header } from './components/layout/Header';
-import { Footer } from './components/layout/Footer';
-import { FloatingElements } from './components/layout/FloatingElements';
-import { SocialMediaBar } from './components/layout/SocialMediaBar';
-import { OrganizationStructuredData, LocalBusinessStructuredData } from './components/seo/StructuredData';
-import { Home } from './pages/Home';
-import { About } from './pages/About';
-import { Services } from './pages/Services';
-import { Portfolio } from './pages/Portfolio';
-import { Blog } from './pages/Blog';
-import { Contact } from './pages/Contact';
+// src/App.tsx
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
+import { ThemeProvider } from "./components/layout/ThemeProvider";
+import { Header } from "./components/layout/Header";
+import { Footer } from "./components/layout/Footer";
+import { FloatingElements } from "./components/layout/FloatingElements";
+import { SocialMediaBar } from "./components/layout/SocialMediaBar";
+import {OrganizationStructuredData,LocalBusinessStructuredData,} from "./components/seo/StructuredData";
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { Services } from "./pages/Services";
+import { Portfolio } from "./pages/Portfolio";
+import { Blog } from "./pages/Blog";
+import { Contact } from "./pages/Contact";
+
+// Analytics tracker - fires page_view on client-side route changes
+import AnalyticsTracker from "./components/Analytics/AnalyticsTracker";
 
 function App() {
   return (
@@ -20,9 +24,16 @@ function App() {
       <ThemeProvider>
         <Router>
           <div className="min-h-screen bg-white dark:bg-gray-900 transition-colors duration-300">
+            {/* Structured data */}
             <OrganizationStructuredData />
             <LocalBusinessStructuredData />
+
+            {/* Site header */}
             <Header />
+
+            {/* Analytics tracker must be inside Router so useLocation() works */}
+            <AnalyticsTracker />
+
             <main>
               <Routes>
                 <Route path="/" element={<Home />} />
@@ -33,6 +44,8 @@ function App() {
                 <Route path="/contact" element={<Contact />} />
               </Routes>
             </main>
+
+            {/* Footer, floating UI and socials */}
             <Footer />
             <FloatingElements />
             <SocialMediaBar />
